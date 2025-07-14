38aa8d5057cdf99d06376cbd044b9ea9b486cf32
zgoodman27 <zgoodman27@gmail.com>
2025-07-14 11:45:48 -0400

    finalized basics of project

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Set up server
-const express = require(express);
+const express = require("express");
 const dotenv = require("dotenv");
 // Set up port
-const port = process.env.port || 8080;
+const PORT = process.env.PORT || 4001;
 // Invoking express
 const app = express();
 // load environmental variables
@@ -11,6 +11,36 @@ dotenv.config();
 // Middleware to parse JSON requests
 app.use(express.json());
 
+// Set up first get route
+app.get("/", (req, res) => {
+  res.send("<h1>Hello, <b>home</b>!</h1>");
+});
+
+// Set up server status page
+app.get("/api/status", (req, res) => {
+  res.send("<h1>Server <b>status is OK</b>.</h1>");
+});
+
+// Set up users page
+app.get("/api/users", (req, res) => {
+  res.send("<h1>List <b>of Users</b>.</h1>");
+});
+
+// Set up blog page
+app.get("/api/blogs", (req, res) => {
+  res.send("<h1>Blog <b>Posts</b>.</h1>");
+});
+
+// Set up :key req
+app.get("/:key", (req, res) => {
+  console.log(req.params);
+});
+
+// Set up :page req
+app.get("/api/:page", (req, res) => {
+  console.log(req.params);
+});
+
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
